572254548d8f1e2e58d69a98b27ca42baa4e4b07
Luka Tchumburidze <luka.tchumburidze@exactprosystems.com>
2020-11-26 14:02:47 +0400

    [TH-1084] changed prefix to "scheme-"

--- src/main/java/com/exactpro/th2/infraoperator/fabric8/configuration/OperatorConfig.java
+++ src/main/java/com/exactpro/th2/infraoperator/fabric8/configuration/OperatorConfig.java
@@ -53,7 +53,7 @@ public enum OperatorConfig {
     public List<String> getNamespacePrefixes () {
         //TODO: actual valid namespace prefixes should be returned
 
-        return Arrays.asList("dev-");
+        return Arrays.asList("scheme-");
     }
 
     public String getRabbitMQSecretName() {
