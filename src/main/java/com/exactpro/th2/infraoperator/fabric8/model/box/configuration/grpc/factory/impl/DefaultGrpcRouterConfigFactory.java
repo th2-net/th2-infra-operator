972efb1e67b03ab5d21bbab2a3c126c7598c46d6
vakhtang.donadze <vakhtang.donadze@exactprosystems.com>
2020-12-07 21:02:53 +0400

    [TH2-1036] change grpc config based on externalBox and hostNetwork flags

--- src/main/java/com/exactpro/th2/infraoperator/fabric8/model/box/configuration/grpc/factory/impl/DefaultGrpcRouterConfigFactory.java
+++ src/main/java/com/exactpro/th2/infraoperator/fabric8/model/box/configuration/grpc/factory/impl/DefaultGrpcRouterConfigFactory.java
@@ -13,6 +13,7 @@
 
 package com.exactpro.th2.infraoperator.fabric8.model.box.configuration.grpc.factory.impl;
 
+import com.exactpro.th2.infraoperator.fabric8.configuration.OperatorConfig;
 import com.exactpro.th2.infraoperator.fabric8.model.box.configuration.grpc.*;
 import com.exactpro.th2.infraoperator.fabric8.model.box.configuration.grpc.factory.GrpcRouterConfigFactory;
 import com.exactpro.th2.infraoperator.fabric8.spec.Th2CustomResource;
@@ -20,13 +21,17 @@ import com.exactpro.th2.infraoperator.fabric8.spec.link.relation.boxes.box.impl.
 import com.exactpro.th2.infraoperator.fabric8.spec.link.relation.boxes.bunch.impl.GrpcLinkBunch;
 import com.exactpro.th2.infraoperator.fabric8.spec.shared.FilterSpec;
 import com.exactpro.th2.infraoperator.fabric8.spec.shared.PinSpec;
+import com.exactpro.th2.infraoperator.fabric8.model.box.configuration.grpc.GrpcEndpointMapping;
 import com.exactpro.th2.infraoperator.fabric8.spec.strategy.resFinder.box.BoxResourceFinder;
 import com.exactpro.th2.infraoperator.fabric8.spec.shared.SchemaConnectionType;
-import com.exactpro.th2.infraoperator.fabric8.util.ExtractUtils;
+import com.exactpro.th2.infraoperator.fabric8.util.CustomResourceUtils;
 import com.exactpro.th2.infraoperator.fabric8.util.SchemeMappingUtils;
 import com.exactpro.th2.infraoperator.fabric8.util.Strings;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,6 +41,9 @@ import java.util.stream.Collectors;
 
 import static com.exactpro.th2.infraoperator.fabric8.model.box.configuration.grpc.StrategyType.FILTER;
 import static com.exactpro.th2.infraoperator.fabric8.model.box.configuration.grpc.StrategyType.ROBIN;
+import static com.exactpro.th2.infraoperator.fabric8.util.ExtractUtils.extractName;
+import static com.exactpro.th2.infraoperator.fabric8.util.ExtractUtils.extractNamespace;
+import static com.exactpro.th2.infraoperator.fabric8.util.JsonUtils.JSON_READER;
 
 
 public class DefaultGrpcRouterConfigFactory implements GrpcRouterConfigFactory {
@@ -44,15 +52,16 @@ public class DefaultGrpcRouterConfigFactory implements GrpcRouterConfigFactory {
 
 
     private static final int DEFAULT_PORT = 8080;
-
     private static final int DEFAULT_SERVER_WORKERS_COUNT = 5;
-
     private static final String ENDPOINT_ALIAS_SUFFIX = "-endpoint";
-
     private static final String SERVICE_CLASS_PLACEHOLDER = "unknown";
-
+    private static final String EXTERNAL_BOX_ALIAS = "externalBox";
+    private static final String HOST_NETWORK_ALIAS = "hostNetwork";
+    private static final String SERVICE_ALIAS = "service";
+    private static final String ENDPOINTS_ALIAS = "endpoints";
+    private static final String GRPC_ALIAS = "grpc";
+    private static final String TRUE_FLAG = "true";
     private static final GrpcServerConfiguration DEFAULT_SERVER = createServer();
-
     private final BoxResourceFinder resourceFinder;
 
 
@@ -64,9 +73,9 @@ public class DefaultGrpcRouterConfigFactory implements GrpcRouterConfigFactory {
     @Override
     public GrpcRouterConfiguration createConfig(Th2CustomResource resource, List<GrpcLinkBunch> grpcActiveLinks) {
 
-        var boxName = ExtractUtils.extractName(resource);
+        var boxName = extractName(resource);
 
-        var boxNamespace = ExtractUtils.extractNamespace(resource);
+        var boxNamespace = extractNamespace(resource);
 
         Map<String, GrpcServiceConfiguration> services = new HashMap<>();
 
@@ -107,25 +116,25 @@ public class DefaultGrpcRouterConfigFactory implements GrpcRouterConfigFactory {
             GrpcLinkBunch link,
             Map<String, GrpcServiceConfiguration> services
     ) {
-
         var naturePinState = getNaturePinState(currentPin.getName(), oppositePinName, link);
 
+        BoxGrpc fromBoxSpec = link.getFrom();
+        String fromBoxName = fromBoxSpec.getBox();
 
-        var fromBoxSpec = link.getFrom();
+        BoxGrpc toBoxSpec = link.getTo();
+        String toBoxName = toBoxSpec.getBox();
 
-        var fromBoxName = fromBoxSpec.getBox();
-
-        var toBoxSpec = link.getTo();
-
-        var toBoxName = toBoxSpec.getBox();
-
+        Th2CustomResource fromBoxResource = resourceFinder.getResource(fromBoxName, namespace);
+        Th2CustomResource toBoxResource = resourceFinder.getResource(toBoxName, namespace);
 
         PinSpec oppositePin;
         if (fromBoxSpec.getPin().equals(currentPin.getName())) {
-            oppositePin = resourceFinder.getResource(toBoxName, namespace).getSpec().getPin(oppositePinName);
+            oppositePin = toBoxResource.getSpec().getPin(oppositePinName);
         } else {
-            oppositePin = resourceFinder.getResource(fromBoxName, namespace).getSpec().getPin(oppositePinName);
+            oppositePin = fromBoxResource.getSpec().getPin(oppositePinName);
         }
+
+        checkForExternal(fromBoxResource, toBoxResource, toBoxSpec);
 
         if (naturePinState.getFromPinName().equals(oppositePin.getName())) {
             resourceToServiceConfig(oppositePin, fromBoxSpec, services);
@@ -140,18 +149,20 @@ public class DefaultGrpcRouterConfigFactory implements GrpcRouterConfigFactory {
             BoxGrpc targetBoxSpec,
             Map<String, GrpcServiceConfiguration> services
     ) {
-        var targetBoxName = targetBoxSpec.getBox();
+        var targetBoxName = targetBoxSpec.isAccessedExternally() ? OperatorConfig.INSTANCE.getK8sUrl() : targetBoxSpec.getBox();
 
         var serviceClass = targetBoxSpec.getServiceClass();
 
         var serviceName = getServiceName(serviceClass);
+
+        var targetPort = targetBoxSpec.isAccessedExternally() ? targetBoxSpec.getPort() : DEFAULT_PORT;
 
         var config = services.get(serviceName);
 
         Map<String, GrpcEndpointConfiguration> endpoints = new HashMap<>(Map.of(
                 targetBoxName + ENDPOINT_ALIAS_SUFFIX, GrpcEndpointConfiguration.builder()
                         .host(targetBoxName)
-                        .port(DEFAULT_PORT)
+                        .port(targetPort)
                         .build()
         ));
 
@@ -236,6 +247,61 @@ public class DefaultGrpcRouterConfigFactory implements GrpcRouterConfigFactory {
                 .workers(DEFAULT_SERVER_WORKERS_COUNT)
                 .port(DEFAULT_PORT)
                 .build();
+    }
+
+    private void checkForExternal(Th2CustomResource fromBox, Th2CustomResource toBox, BoxGrpc targetBox) {
+        Map<String, Object> fromBoxSettings = fromBox.getSpec().getExtendedSettings();
+        Map<String, Object> toBoxSettings = toBox.getSpec().getExtendedSettings();
+        logger.debug("checking external flag for a link [from \"{}\" to \"{}\"]",
+                CustomResourceUtils.annotationFor(fromBox), CustomResourceUtils.annotationFor(toBox));
+        if (isExternal(fromBoxSettings) && !isExternal(toBoxSettings)) {
+            logger.debug("link [from \"{}\" to \"{}\"] needs external gRPC mapping",
+                    CustomResourceUtils.annotationFor(fromBox), CustomResourceUtils.annotationFor(toBox));
+            GrpcEndpointMapping grpcMapping = getGrpcMapping(toBoxSettings);
+            if (grpcMapping != null) {
+                int nodePort = Integer.parseInt(grpcMapping.getNodePort());
+                targetBox.setPort(nodePort);
+                targetBox.setAccessedExternally(true);
+            } else {
+                logger.debug("grpcMapping for resource \"{}\" was null", CustomResourceUtils.annotationFor(toBox));
+            }
+        } else {
+            targetBox.setAccessedExternally(false);
+            logger.debug("link [from \"{}\" to \"{}\"] does NOT need external gRPC mapping",
+                    CustomResourceUtils.annotationFor(fromBox), CustomResourceUtils.annotationFor(toBox));
+        }
+    }
+
+    @SneakyThrows
+    private GrpcEndpointMapping getGrpcMapping(Map<String, Object> boxSettings) {
+        Object service = boxSettings.get(SERVICE_ALIAS);
+        if (service != null) {
+            JsonNode serviceJson = JSON_READER.convertValue(service, JsonNode.class);
+            JsonNode endpointsJson = serviceJson.get(ENDPOINTS_ALIAS);
+            if (endpointsJson != null) {
+                List<GrpcEndpointMapping> grpcEndpointMappings = JSON_READER.convertValue(endpointsJson, new TypeReference<>() {
+                });
+                if (grpcEndpointMappings != null) {
+                    for (GrpcEndpointMapping grpcEndpointMapping : grpcEndpointMappings) {
+                        if (grpcEndpointMapping.getName().equals(GRPC_ALIAS)) {
+                            return grpcEndpointMapping;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean isExternal(Map<String, Object> boxExtendedSettings) {
+        boolean isExternal = false;
+        if (boxExtendedSettings != null) {
+            var hostNetwork = boxExtendedSettings.get(HOST_NETWORK_ALIAS);
+            var externalBox = boxExtendedSettings.get(EXTERNAL_BOX_ALIAS);
+            isExternal = (hostNetwork != null && hostNetwork.toString().equals(TRUE_FLAG));
+            isExternal |= (externalBox != null && externalBox.toString().equals(TRUE_FLAG));
+        }
+        return isExternal;
     }
 
 }
